0812465d5802e9b114c407801ca38679cb2f173d
AnkitAdhikari07 <ankitadhikaricode@gmail.com>
2026-06-24 08:33:10 +0530

    add the tailwindCss and working on the base.html

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./core/templates/**/*.html",
+    "./tasks/templates/**/*.html",
+    "./notes/templates/**/*.html",
+    "./budget/templates/**/*.html",
+    "./travel/templates/**/*.html",
+    "./timetable/templates/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
