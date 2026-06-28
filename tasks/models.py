084b2fb54e4d9599ff68a5535787dc42d84cc21f
AnkitAdhikari07 <ankitadhikaricode@gmail.com>
2026-06-28 18:00:32 +0530

    make dashboard and add one modal task

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('LOW', 'Low'),
+        ('MEDIUM', 'Medium'),
+        ('HIGH', 'High'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='MEDIUM')
+
+    due_date = models.DateField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
